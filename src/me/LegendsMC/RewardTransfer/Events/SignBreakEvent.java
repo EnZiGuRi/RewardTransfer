9d75388fc1612f1b8c1123d3c98a7d7ccf2b729c
EnZiGuRi <admin@legendmc.org>
2016-08-13 19:13:56 +0100

    Fixed console errors on block destroy

--- src/me/LegendsMC/RewardTransfer/Events/SignBreakEvent.java
+++ src/me/LegendsMC/RewardTransfer/Events/SignBreakEvent.java
@@ -1,35 +1,34 @@
 package me.LegendsMC.RewardTransfer.Events;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+
 public class SignBreakEvent implements Listener {
 	@EventHandler
 	public void onSignBreak(BlockBreakEvent event) {
 		Player player = event.getPlayer();
-		Sign sign = (Sign) event.getBlock().getState();
-		if (((event.getBlock().getType() == Material.SIGN)
-				|| (event.getBlock().getType() == Material.SIGN_POST) || (event
-				.getBlock().getType() == Material.WALL_SIGN))
-				&& (((sign.getLine(0).contentEquals(ChatColor.DARK_RED
-						+ "[Transfer]")) && (sign.getLine(1)
-						.contentEquals(ChatColor.DARK_BLUE + "Deposit"))) || (sign
-						.getLine(1).contentEquals(ChatColor.DARK_BLUE
-						+ "Withdraw")))) {
-			if (player.hasPermission("rewardtransfer.sign.remove")) {
-				player.sendMessage(ChatColor.DARK_GREEN + "[RewardTransfer] "
-						+ ChatColor.WHITE + "Sign successfully removed!");
-			} else {
-				event.setCancelled(true);
-				player.sendMessage(ChatColor.DARK_GREEN + "[RewardTransfer] "
-						+ ChatColor.WHITE
-						+ "You dont have permission to remove that sign!");
+		if ((event.getBlock().getState() instanceof Sign)){
+			final Sign sign = (Sign) event.getBlock().getState();
+			if ((sign.getLine(0).contentEquals(ChatColor.DARK_RED
+					+ "[Transfer]")) && ((sign.getLine(1)
+					.contentEquals(ChatColor.DARK_BLUE + "Deposit"))
+					|| (sign.getLine(1).contentEquals(ChatColor.DARK_BLUE
+							+ "Withdraw")))) {
+				if (player.hasPermission("rewardtransfer.sign.remove")) {
+					player.sendMessage(ChatColor.DARK_GREEN
+							+ "[RewardTransfer]: " + ChatColor.WHITE
+							+ "Sign successfully removed!");
+				} else {
+					event.setCancelled(true);
+					player.sendMessage(ChatColor.DARK_GREEN
+							+ "[RewardTransfer] " + ChatColor.WHITE
+							+ "You dont have permission to remove that sign!");
+				}
 			}
 		}
 	}
